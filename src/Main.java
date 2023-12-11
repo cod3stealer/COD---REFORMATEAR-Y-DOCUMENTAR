0269d0ead96ebc6cd7e60a1ecd8cb718641bf59d
cod3stealer <sromerodiaz@danielcastelao.org>
2023-12-11 13:43:43 +0100

    modificaciones

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
+/**
+ * Prueba
+ *
+ * @author Santiago Romero
+ * @version 11.12.2023
+ * */
+
+/**
+ * Import de libreria para el uso de JOptionPane
+ * */
 import javax.swing.*;
 public class Main {
+    /**
+     * Declaracion clase MAIN
+     * */
     public static void main(String[] args){
+        /**
+         * @param showMessageDialog: "Hola"
+         * */
         JOptionPane.showMessageDialog(null,"Hola");
     }
 }
